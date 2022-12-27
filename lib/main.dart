697f54ad34ed8022fd801d9fa013b2bb3ecc0f9e
mohammedNaserHassan <shoakk2015@gmail.com>
2022-12-27 13:47:00 +0200

    second

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'core/values/languages/translations.dart';
 import 'data/providers/local_storage.provider.dart';
 
 void main() {
+  //test  test
   WidgetsFlutterBinding.ensureInitialized();
   GetStorage.init();
   SystemChrome.setPreferredOrientations([
